f29fd8114e9587aa770ed1a0f3e029dfe4dbf916
Тюняева Алёна Александровна <alyonatyunyaeva@gmail.com>
2020-05-30 19:00:24 +0300

    React built site

---
+++ precache-manifest.2b531e549de6da83a0f07d566d39b548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db3638a57a3119c43029403ca70fb2da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1df760f43ef388424d52",
+    "url": "/static/css/main.cb458ce3.chunk.css"
+  },
+  {
+    "revision": "2812efd4a3a9bc43a0dc",
+    "url": "/static/js/2.8c770c87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c770c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df760f43ef388424d52",
+    "url": "/static/js/main.7e1ba560.chunk.js"
+  },
+  {
+    "revision": "af64547e42f85772d4ea",
+    "url": "/static/js/runtime-main.8afef6e7.js"
+  }
+]);
