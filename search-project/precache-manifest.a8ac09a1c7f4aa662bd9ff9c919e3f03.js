aa6767e6ffd322db24c4a84c6f02ba4a123f0c41
 alyona <alyona@MacBook-Pro-mac.local>
2020-07-10 16:44:55 +0300

    + git search repositories

---
+++ search-project/precache-manifest.a8ac09a1c7f4aa662bd9ff9c919e3f03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3957f78f337c6223c0c31625064e4ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d96ddbcf7e99f5856e54",
+    "url": "/static/css/main.3c19c79b.chunk.css"
+  },
+  {
+    "revision": "cd1ba005a2f34740f714",
+    "url": "/static/js/2.e6ead973.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6ead973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96ddbcf7e99f5856e54",
+    "url": "/static/js/main.b58cf8a2.chunk.js"
+  },
+  {
+    "revision": "942ec746d656b70ab778",
+    "url": "/static/js/runtime-main.4bb078ea.js"
+  }
+]);
