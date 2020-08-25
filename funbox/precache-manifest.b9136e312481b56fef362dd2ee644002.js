63ac5d7d7d57e68c39f8ccbfd0489e0bf18d3457
 alyona <alyona@MacBook-Pro-mac.local>
2020-08-25 12:14:16 +0300

    +funbox

---
+++ funbox/precache-manifest.b9136e312481b56fef362dd2ee644002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0dd50e8f3c3968d94fdfb4b0e2db67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "623d232d52ea6c9fb9a9",
+    "url": "/static/css/main.b0e78679.chunk.css"
+  },
+  {
+    "revision": "fbd97a9b0001d4f6ef02",
+    "url": "/static/js/2.da32d21e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.da32d21e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "623d232d52ea6c9fb9a9",
+    "url": "/static/js/main.cb3f7075.chunk.js"
+  },
+  {
+    "revision": "931025b40dcfe92ddcf8",
+    "url": "/static/js/runtime-main.7672b350.js"
+  }
+]);
